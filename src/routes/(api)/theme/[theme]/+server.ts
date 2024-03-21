2ec70e18aa71c2d96c2d79c7f60822ccaf8239df
Fuad Hasan <fuad.cs22@gmail.com>
2024-03-22 01:56:26 +0600

    Refactor styles and remove unused code

--- src/routes/(api)/theme/[theme]/+server.ts
+++ src/routes/(api)/theme/[theme]/+server.ts
@@ -1,7 +1,7 @@
 import { themes } from '$lib/themes.js';
 
 export async function PUT({params, cookies}){
-    console.log(params);
+    
     try{
         if (themes[params.theme]){
             cookies.set('theme', params.theme, { path: '/' });
@@ -13,8 +13,4 @@ export async function PUT({params, cookies}){
     } catch(e){
         return new Response('Error!', { status: 400 });
     }
-}
-
-export function GET(){
-    return new Response('What\'s up? You unlocked a hidded route!👾', { status: 200 });
 }
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,7 @@
-
 <script lang="ts">
     import "$lib/styles/global.scss";
     import { playClickSound } from "$lib/utils";
+    import { navigating } from "$app/stores";
     console.log("Mounted root +layout.svelte");
 
     function removeAttribute(evt: MouseEvent | TouchEvent) {
@@ -11,41 +11,71 @@
     }
 
     function handleClick(event: MouseEvent | TouchEvent) {
-
         const target = event.target as HTMLElement;
 
-        if (target.classList.contains('button-animate')){
+        if (target.classList.contains("button-animate")) {
             target.setAttribute("data-pressed", "true");
             //console.log("add data-pressed");
             //if mouse event, add listener for mouseleave
             if (event instanceof MouseEvent) {
-                target.addEventListener("mouseleave", removeAttribute, { once: true });
+                target.addEventListener("mouseleave", removeAttribute, {
+                    once: true,
+                });
             } else {
                 //if touch event, add listener for touchend
-                target.addEventListener("touchend", removeAttribute, { once: true });
+                target.addEventListener("touchend", removeAttribute, {
+                    once: true,
+                });
             }
         }
-        if (target.classList.contains('play-sound')){
+        if (target.classList.contains("play-sound")) {
             //console.log("play sound");
             playClickSound();
         }
     }
-
-    
 </script>
 
-<svelte:body 
-on:contextmenu|preventDefault
-on:click={handleClick}
-/>
+<svelte:body on:contextmenu|preventDefault on:click={handleClick} />
+
+{#if !!$navigating}
+    <div class="navigationIndicator"></div>
+{/if}
 
 <div class="maincontainer">
     <slot />
 </div>
 
 <style lang="scss">
+    .navigationIndicator {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 3px;
+        background: var(--secondary-dark);
+        animation: navigate 5s;
+        z-index: 100;
+    }
+
+    @keyframes navigate {
+        /* fast width from 0 to 50% in 1s */
+        0% {
+            width: 0;
+        }
+        25% {
+            width: 50%;
+        }
+        /* slow width from 50% to 100% in 5s */
+        90% {
+            width: 95%;
+        }
+        100% {
+            width: 95%;
+        }
+    }
+
     .maincontainer {
         background: rgba(0, 0, 0, 0.6117647059) var(--pattern);
+        transition: background 1s 200ms;
         background-blend-mode: soft-light;
         background-size: cover;
         background-position: center;
